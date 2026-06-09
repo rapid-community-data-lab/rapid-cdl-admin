60bdd11e0707f23d0cb8fe884a6d270eb496ad64
daisyli-qcif <daisy.li@qcif.edu.au>
2026-06-09 16:47:42 +1000

    test: change back to home

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -9,7 +9,7 @@ import logo from '@/assets/logo.svg'
       <el-menu-item index="home">
         <a href="/" class="nav-left">
           <img class="logo" :src="logo" />
-          <span class="home-text">Home Test</span>
+          <span class="home-text">Home</span>
         </a>
       </el-menu-item>
 
